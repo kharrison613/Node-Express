96d9ae230e755ad8150efaa9aca8c4ac95bf4e98
Harrisonk76 <165740611+Harrisonk76@users.noreply.github.com>
2024-09-08 23:35:42 -0700

    update  model file

---
+++ Project-Restful-API/models/userModel.js
@@ -0,0 +1,22 @@
+let users = [];
+
+module.exports = {
+  getAll: () => users,
+  getById: (id) => users.find(user => user.id === id),
+  create: (user) => { users.push(user); return user; },
+  update: (id, updatedUser) => {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      users[index] = { ...users[index], ...updatedUser };
+      return users[index];
+    }
+    return null;
+  },
+  delete: (id) => {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      return users.splice(index, 1)[0];
+    }
+    return null;
+  }
+};
